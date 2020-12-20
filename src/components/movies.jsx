1323abb899c8ea1c35d6adc5add8ad46213eb88c
sd <selma.isil@hotmail.fr>
2020-12-20 14:06:17 +0100

    first commit

---
+++ src/components/movies.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import Pagination from "./common/pagination";
+import { getMovies } from "../services/fakeMovieService";
+import { paginate } from "../utils/paginate";
+
+class Mouvies extends Component {
+  state = {
+    movies: getMovies(),
+    currentPage: 1,
+    pageSize: 4,
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    const { currentPage, pageSize, movies: allMovies } = this.state;
+
+    if (count === 0) return <p>Ther are no movies in database</p>;
+
+    const movies = paginate(allMovies, currentPage, pageSize);
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Mouvies;
+<h2>Mouvies components</h2>;
